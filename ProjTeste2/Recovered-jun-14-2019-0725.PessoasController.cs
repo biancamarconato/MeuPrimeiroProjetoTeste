f9a281928faac36f7e6db4c1c76b8a142aba6012
biancamarconato <bimarconato77@gmail.com>
2019-09-06 09:17:50 -0400

    Crud em asp.net

---
+++ ProjTeste2/Recovered-jun-14-2019-0725.PessoasController.cs
@@ -0,0 +1,74 @@
+﻿using ProjTeste2.Models;
+using System.Web.Mvc;
+using ProjTeste2.Context;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+namespace ProjTeste2.Controllers
+{
+    public class PessoasController : Controller
+    {
+        private readonly ConexaoDb conexaoDb;
+
+        public PessoasController()
+        {
+            conexaoDb = new ConexaoDb();
+        }
+        // GET: Pessoas
+        public ActionResult Index()
+        {
+            IEnumerable<Pessoas> pessoas = conexaoDb.Pessoas;
+            return View(pessoas);
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastro(Pessoas pessoas)
+        {
+            conexaoDb.Entry(pessoas).State = EntityState.Added; 
+            conexaoDb.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Deletar(int pessoaId)
+        {
+            Pessoas pessoa = conexaoDb.Pessoas.Find(pessoaId);
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Deletar(Pessoas pessoa)
+        {
+            Pessoas pessoaAtual = conexaoDb.Pessoas.Find(pessoa.PessoasId);
+            conexaoDb.Entry(pessoaAtual).State = EntityState.Deleted;
+            conexaoDb.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        
+        public ActionResult Editar(int pessoaId)
+        {
+            Pessoas pessoas = conexaoDb.Pessoas.Find(pessoaId);
+            return View(pessoas);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(Pessoas pessoa)
+        {
+            conexaoDb.Entry(pessoa).State = EntityState.Modified;
+            conexaoDb.SaveChanges();
+            return RedirectToAction("Index");
+        }            
+
+        public ActionResult Detalhes(int pessoaId)
+        {
+            Pessoas pessoas = conexaoDb.Pessoas.Find(pessoaId);
+            return View(pessoas);
+        }
+    }
+}
